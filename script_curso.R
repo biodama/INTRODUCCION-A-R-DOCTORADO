93ad85317284e4982711e7653c495b3f4b284e14
biodama <32451290+biodama@users.noreply.github.com>
2024-09-17 13:47:17 +0200

    Add files via upload

--- script_curso.R
+++ script_curso.R
@@ -51,7 +51,7 @@ y <- c( "low" , "high" , "medium" , "low" )
 z <- c(TRUE, FALSE,TRUE,FALSE)
 
 
-# Creación de datos.frame
+# Creación de data.frame
 
 datos <- data.frame(ID=c(1,2,3) ,
 edad=c(10,13,55)  ,
@@ -99,5 +99,198 @@ class(x2.numerica)
 x3 <- c("1","3",5,"10","-")
 
 x3.numerica <- as.numeric(x3) 
+
+
+###############################
+# DATA.FRAME
+###############################
+
+
+dim(datos)
+
+str(datos)
+
+datos
+
+head(datos,n=2)
+
+tail(datos,n=1)
+
+View(datos)
+
+# DANGER
+
+# fix(datos)
+
+
+# ACCESO A ELEMENTOS DE UN DATA.FRAME
+
+datos$edad
+
+datos$"edad"
+
+
+# notación matricial
+
+datos[FILAS , COLUMNAS]
+
+datos [ c(2) , c(3) ]
+
+datos[    , c(2)    ]
+
+datos$edad
+
+datos[    , 2 ]
+
+datos[ c(1,2) , c(2)  ]
+
+datos[ c(1,2) , c(2,3)  ]
+
+
+# Nos da en que directorio estoy
+getwd()
+
+# Establece el directorio de trabajo.
+setwd("")
+
+
+# Establezco el setwd en la carpeta que contiene los fichero a importar
+setwd("C:\\Users\\FGM\\Desktop\\INTRODUCCION-A-R-DOCTORADO-main\\datos\\")
+
+tabla<-read.table("datos.curso1.txt", header=TRUE, sep="\t")
+
+library("openxlsx")
+
+datos <- read.xlsx(xlsxFile="/Users/pfernandezn/Desktop/datos.evaluacion.xlsx",sheet=1)
+
+datos <- read.xlsx("/Users/pfernandezn/Desktop/datos.evaluacion.xlsx",1)
+
+##########################################
+
+datos
+
+datos[   ,   ]
+
+
+# datos$"sexo"=="Mujer" # resultado de esto es un vector lógico (TRUE/FALSE)
+
+criterio <- c(datos$"sexo"=="Mujer") 
+datos [criterio ,     ]
+
+datos [c(datos$"sexo"=="Mujer"),     ]
+
+
+class(datos$"sexo")
+class(datos$"estado.civil")
+
+datos [datos$"sexo"=="Mujer" & datos$"estado.civil"=="Casado" ,1:5 ]
+
+# %in% selecciona sin tener en cuenta los NA (missing)
+datos [datos$"sexo"%in%"Mujer" & datos$"estado.civil"%in%"Casado" ,1:5 ]
+
+
+
+# Lo utizalos para dos cosas principalmente:
+
+# 1) Hacer una sub-base
+
+datos.mujeres <- datos [datos$"sexo"=="Mujer" ,     ]
+
+
+# 2) Recodificacion de valores
+
+datos.new <- datos
+
+datos.new[c(1) , c(3)] <- "Hombre.new"
+
+head(datos.new)
+
+
+# Ejemplo: el paciente de ID=90 es mujer (comprueba)
+
+class(datos.new$"ID")
+
+datos.new[datos.new$"ID"==90, c(3) ] <- c("Hombre")
+
+datos.new[datos.new$"ID"==90, c(3) ]
+
+
+# Ejemplo: ¿cuantos casados hay en la base de datos "datos" ?
+
+
+datos.casados  <-  datos [datos$"estado.civil"=="Casado" ,     ]
+dim(datos.casados)
+
+
+dim(datos [datos$"estado.civil"=="Casado" ,     ])
+
+
+# Ejemplo: Sustituir el valor de Mujer por woman en la variable sexo
+
+datos[datos$"sexo"=="Mujer" ,   3     ]   <-  c("woman")
+
+
+
+# Ejemplo: Sustituir el valor “Bajo” en la variable nivel.estudios por el valor de “B”
+
+datos[datos$"nivel.estudios"=="Bajo", 5] <- c("B")
+
+datos[datos$"nivel.estudios"=="Bajo", c("nivel.estudios")] <- c("B")
+
+
+
+# AÑADIR VARIABLES
+
+datos$"año"   <- c(2024)
+
+
+
+# UNIR BASES DE DATOS
+
+datos_para_unir <- datos[   ,   c(1,2)]
+
+datos_union <- cbind( datos, datos_para_unir)
+
+
+# AÑADIR REGISTROS
+
+datos_para_unir <- datos[ c(1,2,3)  ,    ]
+
+datos_union <- rbind( datos, datos_para_unir)
+
+
+# ELIMINAR REGISTROS
+
+datos.new <- datos[-c(1,2,3) , -c(2) ]
+
+
+# Nombres de las variables de un data.frame
+
+
+names(datos) [  c(1,3)  ] <- c("ID_new","Sex")
+
+
+# Nombres de filas
+
+datos <- read.xlsx(xlsxFile="/Users/pfernandezn/Desktop/datos.evaluacion.xlsx",sheet=1)
+
+datos_new<-datos[c(4,1,7) ,   ]
+
+row.names(datos_new) <- NULL
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
